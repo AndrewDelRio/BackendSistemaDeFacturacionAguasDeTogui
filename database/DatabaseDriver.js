5382862760f93658ce8dfaf8632ee63a991dad29
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-06-15 11:44:51 -0500

    Creation databasedriver file

---
+++ database/DatabaseDriver.js
@@ -0,0 +1,14 @@
+const {Sequelize} = require('sequelize');
+const {dbUserName,dbPassword,dbName,dBHost,dBPort, dbHost, dbPort} = require('../config/config');
+
+const sequelize = new Sequelize(dbName, dbUserName,dbPassword,{
+    host: dbHost,
+    dialect: 'mysql',
+    port:dbPort,
+    logging: false,
+    define:{
+        freezeTableName: true,
+        timestamps:false
+    }
+});
+module.exports = {sequelize};
