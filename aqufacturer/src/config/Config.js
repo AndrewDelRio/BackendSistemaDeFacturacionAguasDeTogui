7fea98ef1520f44e6cad898c729f501e707742f3
Andres Del Rio <74572749+AndrewDelRio@users.noreply.github.com>
2023-06-15 09:43:45 -0500

    Delete aqufacturer directory

--- aqufacturer/src/config/Config.js
+++
@@ -1,4 +0,0 @@
-const title_System = "Sistema de Facturacion"
-const name_factory = "Empresa de Servicios Publicos de Togüí"
-const slogan_factory = "Aguas de Togüí"
-const url_principal_page = "http://espaguasdetogui-boyaca.gov.co/"
