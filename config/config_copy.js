4eced62816ee23983cec0d834e8208eef43afd32
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-06-20 11:52:28 -0500

    Add settings to localhost

--- config/config_copy.js
+++ config/config_copy.js
@@ -1,7 +1,7 @@
-const dbUserName = 'adm_db_ATSAESP';
-const dbPassword = 'adminATSAESP';
+const dbUserName = 'root';
+const dbPassword = 'MYSQL123root-';
 const dbName = 'public_services_factory_togui_boy';
-const dbHost = '127.0.0.1';
+const dbHost = 'localhost';
 const dbPort = '3306';
 
 process.env.JWT_SIGNATURE = process.env.JWT_SIGNATURE || 'developmenttokensignature';
