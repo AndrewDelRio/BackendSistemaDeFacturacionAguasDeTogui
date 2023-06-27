918d75e7638d9d9cb110a8a667053006bab93f1f
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-06-27 15:15:43 -0500

    Add update register to the login to the database

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -6,6 +6,7 @@ const {roleModel} = require("../models/RoleModel");
 const jwt = require("jsonwebtoken");
 const sha256 = require("sha256");
 const { QueryTypes } = require('@sequelize/core');
+const LocalDate = require("localdate");
 
 /**
  * Login function
@@ -13,6 +14,15 @@ const { QueryTypes } = require('@sequelize/core');
  * Out: Name, rol, last_access 
  */
 loginController.post("/login", (req,res) =>{
+    let ts = new Date();
+    let day = ("0" + ts.getDate()).slice(-2);
+    let month = ("0" + (ts.getMonth() + 1)).slice(-2);
+    let year = ts.getYear();
+    let hour = ts.getHours();
+    let minutes = ts.getMinutes();
+    let seconds = ts.getSeconds();
+    let ip_address = req.ip;
+    let date_to_database = year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':' + seconds;
  let condition = {
     where:{
         access_email_user: req.body.email
@@ -23,7 +33,7 @@ loginController.post("/login", (req,res) =>{
         return res.status(200).json({
             ok:false,
             error: "Not found",
-            message: "Correo no registrado en el sistema"
+            message: "Mail not registered in the system"
         })
     } else {
         var password = req.body.password;
@@ -31,18 +41,30 @@ loginController.post("/login", (req,res) =>{
         if (!state) {
             return res.status(200).json({
                 ok:false,
-                error: "Incorrect Password",
-                message: "Usuario y/o contraseña incorrectos"
+                error: "Incorrect email/Password",
+                message: "Wrong username and/or password"
             });
         } else {
             let token = jwt.sign({user:result}, process.env.JWT_SIGNATURE, {
                 expiresIn: process.env.JWT_EXP_TIME,
             });
-            return res.status(200).json({
-                ok:true,
-                token:token,
-                message:{id_system_user: result.id_system_user,names_user:result.names_user,active_state: result.active_state, id_role:result.id_rol,last_access_date: result.last_access_date,last_access_ip_address: result.last_access_ip_address }
-            });
+            systemUserModel.update(
+                {last_access_date:date_to_database, last_access_ip_address: ip_address },
+                {where: {id_system_user: result.id_system_user}}
+                ).then(rest =>{
+                    return res.status(200).json({
+                        ok:true,
+                        token:token,
+                        message:{id_system_user: result.id_system_user,names_user:result.names_user,active_state: result.active_state, id_role:result.id_rol,last_access_date: result.last_access_date,last_access_ip_address: result.last_access_ip_address }
+                    });
+                }).catch(err =>{
+                    return res.status(200).json({
+                        ok:false,
+                        error: "Update Error",
+                        message: "Error updating the entry record in the database"
+                    })
+                })
+                
         }
     }
  }).catch((err)=>{
