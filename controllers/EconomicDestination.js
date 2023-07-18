17b5a6ba1bce757e2fe4977970108efd13e243d2
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-07-18 11:40:30 -0500

    Modyfing economic destine route

--- controllers/EconomicDestination.js
+++ controllers/EconomicDestination.js
@@ -2,7 +2,7 @@ const express = require('express');
 const economicDestinationController = express();
 const { JWTokenVerification } = require('../middleware/Authentication');
 
-economicDestinationController.get('/getEconomiclaDestine', [JWTokenVerification], (req, res) => {
+economicDestinationController.get('/getEconomicalDestine', [JWTokenVerification], (req, res) => {
     let economicDestination = ['Habitacional', 'Industrial', 'Comercial', 'Agropecuario', 'Minero', 'Cultural', 'Recreacional', 'Salubridad', 'Institucional o Estado', 'Mixto', 'Otros'];
     return res.status(200).json({ ok: true, result: economicDestination });
 });
