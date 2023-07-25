fba2b39e97cbc7c4de1b3a1fefa59d79f1f95bae
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-07-25 15:07:38 -0500

    Adding field to enrollment model

--- models/EnrollmentModel.js
+++ models/EnrollmentModel.js
@@ -1,36 +1,40 @@
-const {sequelize} = require('../database/databaseDriver');
-const {DataTypes} = require('sequelize');
+const { sequelize } = require('../database/databaseDriver');
+const { DataTypes } = require('sequelize');
 
 const tableName = 'enrollments';
 const options = {};
 const attributes = {
-    id_enrollment:{
+    id_enrollment: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        allowNull:false,
-        autoIncrement:true
+        allowNull: false,
+        autoIncrement: true
     },
-    date_adward:{
+    date_adward: {
         type: DataTypes.DATE,
-        allowNull:true
+        allowNull: true
     },
-    state_enrollment:{
-        type:DataTypes.STRING(4),
-        allowNull:true
+    state_enrollment: {
+        type: DataTypes.STRING(4),
+        allowNull: true
     },
-    id_financing:{
-        type:DataTypes.INTEGER,
-        allowNull:false
+    id_financing: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     },
-    id_property_number:{
-        type:DataTypes.STRING(50),
-        allowNull:false
+    id_property_number: {
+        type: DataTypes.STRING(50),
+        allowNull: false
     },
-    id_subscriber:{
-        type:DataTypes.INTEGER,
-        allowNull:false
+    id_subscriber: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    cuotes_financing: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
 };
 
-const enrollmentModel = sequelize.define(tableName,attributes,options);
-module.exports = {enrollmentModel};
+const enrollmentModel = sequelize.define(tableName, attributes, options);
+module.exports = { enrollmentModel };
