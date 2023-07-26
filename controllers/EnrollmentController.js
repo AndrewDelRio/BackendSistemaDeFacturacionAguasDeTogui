29e9e2d8d0aa6349617069ef182722dcfde3b5db
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-07-26 10:50:07 -0500

    Adding enrollment functions

--- controllers/EnrollmentController.js
+++ controllers/EnrollmentController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const enrollmentController = express();
 const { Op } = require('sequelize');
 const { enrollmentModel } = require('../models/EnrollmentModel');
+const { historicalEnrollmentModel } = require('../models/HistoricalEnrollmentModel');
 const { JWTokenVerification } = require('../middleware/Authentication');
 const { QueryTypes } = require('@sequelize/core');
 
@@ -30,6 +31,46 @@ enrollmentController.get('/getEnrollment/:idEnrollment', [JWTokenVerification], 
             message: "Error to try connect to database"
         })
     })
+});
+
+enrollmentController.post('/addEnrollment', [JWTokenVerification], (req, res) => {
+    let newEnrollment = enrollmentModel.build({
+        id_enrollment: null,
+        date_adward: req.body.date_adward,
+        state_enrollment: 'Adjudicada',
+        id_financing: Number(req.body.id_financing),
+        id_property_number: req.body.id_property_number,
+        id_subscriber: Number(req.body.id_subscriber),
+        cuotes_financing: Number(req.body.cuotes_financing)
+    });
+    newEnrollment.save().then((result) => {
+        if (result) {
+            let newHistoricalEnrollment = historicalEnrollmentModel.build({
+                id_historical_enrollment: null,
+                date_operation: new Date(),
+                type_operation: 'Adjudicada',
+                observations: '',
+                id_enrollment: result.id_enrollment
+            });
+            newHistoricalEnrollment.save().then((historicalResult) => {
+                if (historicalResult) {
+
+                    res.status(200).json({ ok: true, message: 'The enrollment has been added to the system', result: result.id_enrollment });
+                } else {
+                    res.status(500).json({ ok: false, message: 'Error to try add the historical enrollment', error: err });
+                }
+            }).catch((err) => {
+                res.status(500).json({ ok: false, message: 'Error to try to add the enrollment', error: err });
+            })
+        }
+    }).catch((err) => {
+        res.status(500).json({ ok: false, message: 'Error to try connect to the database', error: err });
+    })
+})
+
+enrollmentController.get('/getEnrollmentStates', [JWTokenVerification], (req, res) => {
+    let enrollmentsStateList = ['Adjudicada', 'En instalación', 'Activa', 'Suspensión impuesta', 'Suspensión temporal', 'Cancelada'];
+    return res.status(200).json({ ok: true, result: enrollmentsStateList });
 })
 
 module.exports = { enrollmentController }
--- models/HistoricalEnrollmentModel.js
+++ models/HistoricalEnrollmentModel.js
@@ -1,32 +1,32 @@
-const {sequelize} = require('../database/databaseDriver');
-const {DataTypes} = require('sequelize');
+const { sequelize } = require('../database/databaseDriver');
+const { DataTypes } = require('sequelize');
 
 const tableName = 'historical_enrollment';
 const options = {};
 const attributes = {
-    id_hstorical_enrollment:{
+    id_hstorical_enrollment: {
         type: DataTypes.BIGINT,
         primaryKey: true,
-        allowNull:false,
-        autoIncrement:true
+        allowNull: false,
+        autoIncrement: true
     },
-    date_operation:{
-        type: DataTypes.DATETIME,
-        allowNull:false
+    date_operation: {
+        type: DataTypes.DATE,
+        allowNull: false
     },
-    type_operation:{
-        type:DataTypes.STRING(45),
-        allowNull:false
+    type_operation: {
+        type: DataTypes.STRING(45),
+        allowNull: false
     },
-    observations:{
-        type:DataTypes.STRING(2000),
-        allowNull:true
+    observations: {
+        type: DataTypes.STRING(2000),
+        allowNull: true
     },
-    id_enrollment:{
-        type:DataTypes.INTEGER,
-        allowNull:false
+    id_enrollment: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
 };
 
-const historicalEnrollmentModel = sequelize.define(tableName,attributes,options);
-module.exports = {historicalEnrollmentModel};
+const historicalEnrollmentModel = sequelize.define(tableName, attributes, options);
+module.exports = { historicalEnrollmentModel };
