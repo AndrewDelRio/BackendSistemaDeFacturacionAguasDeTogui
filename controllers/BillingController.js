fc724a62a0309364808b1e151708e607a8e45522
Andres Fabian Simbaqueba Del Rio <andres.simbaqueba@uptc.edu.co>
2023-08-15 14:02:40 -0500

    Adding billing controller

--- controllers/BillingController.js
+++ controllers/BillingController.js
@@ -24,4 +24,23 @@ billingController.get('/getInvoiceLastPeriod/:id_period', [JWTokenVerification],
     })
 })
 
+billingController.post('/getPaidBillings', [JWTokenVerification], (req, res) => {
+    const billingList = req.body.payment_list;
+    let counterUpdatedBillings = 0;
+    const query = 'CALL sp_update_invoice(:id_bill, :invoice_payment_date)';
+    billingList.map(function (billing) {
+        billModel.sequelize.query(query, {
+            type: QueryTypes.EXEC,
+            replacements: { id_bill: billing.id_bill, invoice_payment_date: billing.invoice_payment_date }
+        }).then((result) => {
+            counterUpdatedBillings++;
+            if (counterUpdatedBillings === billingList.length) {
+                return res.status(200).json({ ok: true, message: 'Bills updated' });
+            }
+        }).catch((err) => {
+            return res.status(400).json({ ok: false, err: err, message: "Error to try connect to database" })
+        })
+    })
+})
+
 module.exports = { billingController }
